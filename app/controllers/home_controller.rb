cfc6c517e897b9c0d0cfba5bea55e9b7459cb0e3
Mervyl Canlas <mervylcanlas@gmail.com>
2021-07-28 20:17:21 +0800

    Added IEX API at Home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
     def index
+
+        @client = IEX::Api::Client.new(
+            publishable_token: 'pk_8e835bb9a3e1499480d3372f8afb35f7',
+            secret_token: 'sk_40d28152931a462c81ecdc9fe9c7268f',
+            endpoint: 'https://cloud.iexapis.com/v1'
+            )
+        
     end
 end
