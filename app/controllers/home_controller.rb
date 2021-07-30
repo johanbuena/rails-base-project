da779a889b62b5bd5d2e3734b3b1a430ce017dbd
Mervyl Canlas <mervylcanlas@gmail.com>
2021-07-30 19:19:39 +0800

    Updated home#index with API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,30 @@
 class HomeController < ApplicationController
     def index
 
-        @client = IEX::Api::Client.new(
-            publishable_token: 'pk_8e835bb9a3e1499480d3372f8afb35f7',
-            secret_token: 'sk_40d28152931a462c81ecdc9fe9c7268f',
-            endpoint: 'https://cloud.iexapis.com/v1'
-            )
-        
+        if current_user.present?
+            client = IEX::Api::Client.new(
+                publishable_token: 'pk_8e835bb9a3e1499480d3372f8afb35f7',
+                secret_token: 'sk_40d28152931a462c81ecdc9fe9c7268f',
+                endpoint: 'https://cloud.iexapis.com/v1'
+                )
+            
+            stocklist = ['amd',
+                        'f',
+                        'amc',
+                        'aapl',
+                        'pcg',
+                        'ge',
+                        'clf',
+                        'uber',
+                        'btbt',
+                        'wish']
+
+            @stocklist_view = []
+
+            stocklist.each do |e|
+                @stocklist_view.push(client.quote(e))
+            end
+        end
+
     end
 end
