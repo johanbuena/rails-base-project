feb835ad29122f4890b9d900d064ac8007bd1635
Mervyl Canlas <mervylcanlas@gmail.com>
2021-08-02 19:29:53 +0800

    removed api call inhome controllerr

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,7 @@
 class HomeController < ApplicationController
     before_action :user_or_admin
     def index
-        if current_user.present?
-            client = IEX::Api::Client.new(
-                publishable_token: 'pk_8e835bb9a3e1499480d3372f8afb35f7',
-                secret_token: 'sk_40d28152931a462c81ecdc9fe9c7268f',
-                endpoint: 'https://cloud.iexapis.com/v1'
-                )
-            
-            stocklist = ['amd',
-                        'f',
-                        'amc',
-                        'aapl',
-                        'pcg',
-                        'ge',
-                        'clf',
-                        'uber',
-                        'btbt',
-                        'wish']
-
-            @stocklist_view = []
-
-            stocklist.each do |e|
-                @stocklist_view.push(client.quote(e))
-            end
-        end
+        
     end
 
     def user_or_admin
