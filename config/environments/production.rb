f11d72259dcc07579d96e009e63f5d8bdc562b38
Mervyl Canlas <mervylcanlas@gmail.com>
2021-08-14 10:52:50 +0800

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails_project_production"
 
+  #config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://secret-gorge-80425.herokuapp.com', port: 3000 }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'mail.google.com',
+  user_name:            Rails.application.credentials.gmail[:user_name],
+  password:             Rails.application.credentials.gmail[:password],
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
